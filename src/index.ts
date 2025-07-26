3b5704b2b764a15c62e8f07dc0a2bde4bc76ad43
bmen25124 <bmen25124@gmail.com>
2025-07-26 22:55:28 +0300

    Prettier

--- src/index.ts
+++ src/index.ts
@@ -6,47 +6,47 @@ import { exec } from 'child_process';
 const ID = 'mcp';
 
 async function init(router: Router): Promise<void> {
-    mcpInit(router);
-    // @ts-ignore
-    router.post('/open-settings', (request: Request, response) => {
-        // Make sure file is exist
-        readMcpSettings(request.user.directories);
+  mcpInit(router);
+  // @ts-ignore
+  router.post('/open-settings', (request: Request, response) => {
+    // Make sure file is exist
+    readMcpSettings(request.user.directories);
 
-        // Open in explorer
-        const platform = process.platform;
-        const filePath = path.join(request.user.directories.root, MCP_SETTINGS_FILE);
+    // Open in explorer
+    const platform = process.platform;
+    const filePath = path.join(request.user.directories.root, MCP_SETTINGS_FILE);
 
-        let command;
-        switch (platform) {
-            case 'darwin':  // macOS
-                command = `open -R "${filePath}"`;
-                break;
-            case 'win32':   // Windows
-                command = `explorer /select,"${filePath}"`;
-                break;
-            default:        // Linux and others
-                command = `xdg-open "${filePath.replace(/[^/]*$/, '')}"`;
-                break;
-        }
+    let command;
+    switch (platform) {
+      case 'darwin': // macOS
+        command = `open -R "${filePath}"`;
+        break;
+      case 'win32': // Windows
+        command = `explorer /select,"${filePath}"`;
+        break;
+      default: // Linux and others
+        command = `xdg-open "${filePath.replace(/[^/]*$/, '')}"`;
+        break;
+    }
 
-        exec(command, (_error: Error | null) => {
-            response.send({});
-        });
+    exec(command, (_error: Error | null) => {
+      response.send({});
     });
+  });
 }
 
 interface PluginInfo {
-    id: string;
-    name: string;
-    description: string;
+  id: string;
+  name: string;
+  description: string;
 }
 
 export default {
-    init,
-    exit: (): void => { },
-    info: {
-        id: ID,
-        name: 'MCP Server',
-        description: 'Allows you to connect to an MCP server and execute tools',
-    } as PluginInfo,
+  init,
+  exit: (): void => {},
+  info: {
+    id: ID,
+    name: 'MCP Server',
+    description: 'Allows you to connect to an MCP server and execute tools',
+  } as PluginInfo,
 };
